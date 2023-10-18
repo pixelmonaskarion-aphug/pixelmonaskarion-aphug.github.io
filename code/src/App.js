841f5e958c791c2d10c83d68be2f4be59569c187
Christopher Huntwork <christopher@huntwork.net>
2023-10-18 09:21:02 -0700

    updated some text

--- code/src/App.js
+++ code/src/App.js
@@ -3,16 +3,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+		<Header/>
 		<img className='Arrow' src="Arrow.svg"></img>
 		<TOC/>
 		<header className="App-header">
 			<p className='Title ExtraBig'>AP Human Geo</p>
 		</header>
 		<div className='App-body'>
-        <p className='Title' id="background_information">THis is what the website looks like</p>
-          <p className='Title Smaller'>I don't know what to put on it tho</p>
-            <p className='BodyText'>blah bah</p>
- </div>
+			<p className='Title' id="background_information">Website</p>
+			<p className='Title Smaller'>This is my website for AP Human Geography</p>
+			<p className='BodyText'>this part will change when I add something else from Unit 1 and 2</p>
+ 		</div>
     </div>
   );
 }
@@ -23,6 +24,15 @@ function TOC() {
 		<p>Table of Contents</p>
 		<a href='#background_information'> idk</a>
 	</div>
+	)
+}
+
+function Header() {
+	return (
+		<div className='Header'>
+			<span>Pages</span>
+			<a href='#background_information'> idk</a>
+		</div>
 	)
 }
 
