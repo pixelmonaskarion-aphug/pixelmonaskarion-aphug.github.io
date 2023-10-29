4f9d32ec78d869a4b7b1e9ebcfa4284fad3e7913
Chistopher Huntwork <christopher@huntwork.net>
2023-10-29 11:52:08 -0700

    picture formatting

--- code/src/Unit1.js
+++ code/src/Unit1.js
@@ -3,10 +3,16 @@ function Unit1() {
         <div className='App-body'>
             <p className='Title'>Unit 1: Thinking Geographically</p>
             <p className='Title Smaller'>Maps</p>
-            <img src="https://www.freeworldmaps.net/asia/southkorea/southkorea-map-physical.jpg"/>
-            <img src="https://www.freeworldmaps.net/centralamerica/panama/panama-map-physical.jpg"/>
-            <img src="https://pbs.twimg.com/media/FR5zrEGXsAA1ZTq.png"/>
-            <img src="https://maps.lib.utexas.edu/maps/americas/panama_pop_1981.jpg"/>
+            <div className="Flex">
+                <div className="FlexHalf">
+                    <img className="HalfScreenImage" src="https://www.freeworldmaps.net/asia/southkorea/southkorea-map-physical.jpg"/>
+                    <img className="HalfScreenImage" src="https://www.freeworldmaps.net/centralamerica/panama/panama-map-physical.jpg"/>
+                </div>
+                <div className="FlexHalf">
+                    <img className="HalfScreenImage" src="https://pbs.twimg.com/media/FR5zrEGXsAA1ZTq.png"/>
+                    <img className="HalfScreenImage" src="https://maps.lib.utexas.edu/maps/americas/panama_pop_1981.jpg"/>
+                </div>
+            </div>
             <p className='Title Smaller'>Four Level Anaylsis</p>
             
         </div>
