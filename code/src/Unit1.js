2daedabb0858d4f64fa8197d944189342abccc89
Christopher Huntwork <christopher@huntwork.net>
2023-10-29 11:29:47 -0700

    unit1 pictures

--- code/src/Unit1.js
+++ code/src/Unit1.js
@@ -1,5 +1,16 @@
 function Unit1() {
-    return <p>Unit 1</p>;
+    return (
+        <div className='App-body'>
+            <p className='Title'>Unit 1: Thinking Geographically</p>
+            <p className='Title Smaller'>Maps</p>
+            <img src="https://www.freeworldmaps.net/asia/southkorea/southkorea-map-physical.jpg"/>
+            <img src="https://www.freeworldmaps.net/centralamerica/panama/panama-map-physical.jpg"/>
+            <img src="https://pbs.twimg.com/media/FR5zrEGXsAA1ZTq.png"/>
+            <img src="https://maps.lib.utexas.edu/maps/americas/panama_pop_1981.jpg"/>
+            <p className='Title Smaller'>Four Level Anaylsis</p>
+            
+        </div>
+        );
 }
 
 export default Unit1;
