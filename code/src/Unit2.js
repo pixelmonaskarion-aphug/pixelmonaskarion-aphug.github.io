c9ab5dc7556f6c49eaee5c6232c4ca211d8e344e
Christopher Huntwork <christopher@huntwork.net>
2023-11-11 10:57:51 -0800

    unit 2 4la

--- code/src/Unit2.js
+++ code/src/Unit2.js
@@ -7,7 +7,18 @@ function Unit2() {
         <a className="Source" href="https://pubmed.ncbi.nlm.nih.gov/31533588/">National Library of Medicine</a>
         <p className='Title Smaller'>Panama Population pyramid</p>
         <div className="Image">
-            <img src="https://images.populationpyramid.net/capture/?selector=%23pyramid-share-container&url=https%3A%2F%2Fwww.populationpyramid.net%2Fpanama%2F2019%2F%3Fshare%3Dtrue"></img>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4a/South_Korea_Population_Pyramid.svg/1200px-South_Korea_Population_Pyramid.svg.png"></img>
+        </div>
+        <div className="FLAnalysis">
+            <p className='Title'>Four Level Anaylsis</p>
+            <p className='Title Smaller'>Step One</p>
+            <p>A population pyramid of South Korea from 2022, and a natalist policy from South Korea.</p>
+            <p className='Title Smaller'>Step Two</p>
+            <p>The natalist policy is in favor of births. The government has been trying to increase births since 2005. The biggest part of the population is from ages 40-60. Around age 20, there is a sharp decline in people. And around age 10 there is another less steep but longer decrease in people.</p>
+            <p className='Title Smaller'>Step Three</p>
+            <p>Around the year 2000, the graph shows that births started decreasing heavily. After about 5 years this stopped happening for a short period of time. These events line up with the governments natalist policies, the decreasing stopped a little after 2005, when the policy was put in place. Unfortunately, this did not last for long and the decrease continued around 2016.</p>
+            <p className='Title Smaller'>Step Four</p>
+            <p>The policy enacted by the government of South Korea worked to stabilize population in the short term, but it failed in the long term. As the graph shows, if nothing changes, the population of South Korea will decrease until there are very little or no births. New policies need to be created to prevent this from happening.</p>
         </div>
     </div>
     );
