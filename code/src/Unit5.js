f665fb624ff66905f29a09a9a0b6b0e987a9f6bb
Christopher Huntwork <christopher@huntwork.net>
2024-03-01 20:16:56 -0800

    samsung

--- code/src/Unit5.js
+++ code/src/Unit5.js
@@ -13,9 +13,13 @@ function Unit5() {
             <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Napa_cabbages.png/440px-Napa_cabbages.png"></img>
             <p className='Title Smaller'>Garlic</p>
             <p>Garlic is part of many dishes in South Korean cuisine. South Korea is one of the biggest exporter of Garlic in the world. </p>
-            <img src="https://ogden_images.s3.amazonaws.com/www.iamcountryside.com/images/sites/1/2021/09/19114817/AdobeStock_364056397-scaled-e1662410682321.jpeg"></img>
+            <img className="BigAssChillies" src="https://ogden_images.s3.amazonaws.com/www.iamcountryside.com/images/sites/1/2021/09/19114817/AdobeStock_364056397-scaled-e1662410682321.jpeg"></img>
             <p className='Title Smaller'>Chillies</p>
+            <p>Chillies are popular in dishes like kimchi, stews, and stir-fries, it's very important for the cuisine of South Korea.</p>
             <img className="BigAssChillies" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/Drying_Korean_chillies.jpg/2880px-Drying_Korean_chillies.jpg"></img>
+            <p className='Title Smaller'>Integrated Circuits</p>
+            <p>Integrated circuits are the #1 export exported item from South Korea. Mostly because Samsung is based there and makes a lot of electronics.</p>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/NXP_PCF8577C_LCD_driver_with_I%C2%B2C_%28Colour_Corrected%29.jpg/440px-NXP_PCF8577C_LCD_driver_with_I%C2%B2C_%28Colour_Corrected%29.jpg"></img>
         </div>
     </div>
     );
